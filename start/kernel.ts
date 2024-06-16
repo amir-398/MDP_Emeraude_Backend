11ec3b36bfcca8a054d781ea7748924912a99e2d
amir-398 <amir.398@hotmail.fr>
2024-06-16 23:42:10 +0200

    chore: Update middleware import paths for container bindings

--- start/kernel.ts
+++ start/kernel.ts
@@ -23,7 +23,7 @@ server.errorHandler(() => import('#exceptions/handler'))
  * the request URL.
  */
 server.use([
-  () => import('#policies/middleware/container_bindings_middleware'),
+  () => import('#middleware/container_bindings_middleware'),
   () => import('#middleware/force_json_response_middleware'),
   () => import('@adonisjs/cors/cors_middleware'),
 ])
