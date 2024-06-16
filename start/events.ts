301b26c70dd4cd2fe343f3305413d3c132deba9f
amir-398 <amir.398@hotmail.fr>
2024-06-16 02:32:35 +0200

    chore: Update notification listener to handle WebSocket server availability and notification parsing

--- start/events.ts
+++ start/events.ts
@@ -2,6 +2,7 @@ import app from '@adonisjs/core/services/app'
 import emitter from '@adonisjs/core/services/emitter'
 import Notifications from '../app/services/notifications.js'
 const NotificationListener = () => import('#listeners/notification_listener')
+
 app.ready(() => {
   Notifications.connect()
 })
